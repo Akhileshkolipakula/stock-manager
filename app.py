02c0fe32ed69e5fbf1c829e9f74d84c43069a06d
Akhilesh-Kolipakula <akhilesh.kolipakula@atos.net>
2026-02-02 22:09:10 +0530

    removed dotenv

--- app.py
+++ app.py
@@ -6,9 +6,6 @@ import pandas as pd
 import hashlib
 from datetime import date
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 DB_HOST = os.getenv("DB_HOST")
 DB_NAME = os.getenv("DB_NAME")
